f853cf4074022817103797664e1d16f8d78a1ce8
mgyang53 <mg960503@gmail.com>
2024-10-23 17:24:41 +0900

    기업공지 리스트 css 모듈화

--- src/pages/common/notice/list/listFunc.js
+++ src/pages/common/notice/list/listFunc.js
@@ -1,8 +1,14 @@
 // 기능 추가
 const listFunc = async () => {
-  // const title = document.querySelector('#title');
-  // const changeLink = () => (location.href = '/signUp');
-  // title.addEventListener('click', changeLink);
+  const curPath = window.location.pathname;
+  const listItems = document.querySelectorAll('[data-item-id]');
+
+  listItems.forEach((item) => {
+    item.addEventListener('click', () => {
+      // window.location.href = `${curPath}/view/${item.dataset.itemId}`;
+      window.location.href = `${curPath}/view`;
+    });
+  });
 };
 
 export default listFunc;
