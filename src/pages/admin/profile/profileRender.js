8e3ab674421af94560db6de3f513d7f0ad6d1ed0
박찬희 <fhfh54321@motionimaging.net>
2024-10-24 17:11:37 +0900

    프로필마크업

--- src/pages/admin/profile/profileRender.js
+++ src/pages/admin/profile/profileRender.js
@@ -3,8 +3,21 @@ import style from './profile.module.css';
 const profileRender = () => `
   <div class="${style.container}">
     <div class="${style.profileBox}">
-      <botton class="${style.btn}">프로필 수정</botton>
-    </div>
+      <div class="${style.contentsBox}">
+        <botton class="${style.btn}">프로필 수정</botton>
+        <div class="${style.profileItem}">
+          <div class="${style.division}">임직원</div>
+          <div class="${style.profileImg}">
+          <image src="/public/vite.svg" alt="프로필 사진"></image>
+          </div>
+          <div class="${style.name}">홍길동</div>
+          <div class="${style.grade}">대리</div>
+          </div>
+          <div class="${style.footer}">
+            <span>test@naver.com</span>
+            <span>010-1234-5678</span>
+      </div>
+        </div>
   </div>
   `;
 
