0eda42845a831758b0bb8018cf1d4476b2659a3f
박찬희 <fhfh54321@motionimaging.net>
2024-10-24 12:06:43 +0900

    라우팅후마크업중

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,6 @@
 import SignIn from '../pages/common/signIn';
 import SignUp from '../pages/common/signUp';
-
+import Profile from '../pages/admin/profile';
 // 페이지 구조 저장 + 렌더링 페이지 정리
 const routes = {
   //common
@@ -8,8 +8,9 @@ const routes = {
   '/signUp': SignUp,
 
   //admin
-
+  '/admin/profile':Profile,
   //user
+  
 };
 
 export default routes;
