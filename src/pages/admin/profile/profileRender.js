eb168c42e891dc97ea5a96ff40b7d5293c2f97c0
박찬희 <fhfh54321@motionimaging.net>
2024-10-24 14:36:23 +0900

    마크업진행중

--- src/pages/admin/profile/profileRender.js
+++ src/pages/admin/profile/profileRender.js
@@ -1,8 +1,8 @@
 import style from './profile.module.css';
 // 페이지 렌더링
 const profileRender = () => `
-  <div class="${style.body}">
-    <div class="${style.profileWrapper}">
+  <div class="${style.container}">
+    <div class="${style.profileBox}">
       <botton class="${style.btn}">프로필 수정</botton>
     </div>
   </div>
