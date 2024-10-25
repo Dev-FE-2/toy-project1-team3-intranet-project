9ca6114ad47d7745d345d7efb88183002e4cf74f
최영애 <choiaeyoung1001@gmail.com>
2024-10-25 16:54:39 +0900

    feat : 부재신청 모달 추가
    부재신청 모달 추가 및 마크업 작업 완료

--- src/pages/user/absence/absenceFunc.js
+++ src/pages/user/absence/absenceFunc.js
@@ -1,10 +1,23 @@
 // 기능 추가
 const absenceFunc = async () => {
-  const title = document.querySelector('#title');
+  const modal = document.querySelector('.modal');
+  const modalBack = document.querySelector('.modalBack');
+  const modalOpen = document.querySelector('.absenceBtn');
+  const modalClose = document.querySelector('.closeBtn');
 
-  const changeLink = () => (location.href = '/user/absence');
-
-  title.addEventListener('click', changeLink);
+  modalOpen.addEventListener('click',function(){
+    //display 속성을 block로 변경
+    modal.style.display = 'block';
+  });
+  //닫기 버튼을 눌렀을 때 모달팝업이 닫힘
+  modalClose.addEventListener('click',function(){
+    //display 속성을 none으로 변경
+      modal.style.display = 'none';
+  });
+  modalBack.addEventListener('click',function(){
+    //display 속성을 none으로 변경
+      modal.style.display = 'none';
+  });
 };
 
 export default absenceFunc;
