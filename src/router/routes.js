28b2eb1525d417d651b790a36477653ddab417f1
mgYang53 <50770004+mgYang53@users.noreply.github.com>
2024-10-31 19:01:46 +0900

    기업공지 상세 페이지 기능 개발 (#55)
    * feat : 라우팅 동적 경로 처리 추가
    
    - 상세 페이지 접속을 위해 동적 경로 처리
    
    * feat : 기업공지 상세 관련 API 추가
    
    - 기업공지 상세 데이터 조회 API 추가
    - 기업공지 상세 데이터 삭제 API 추가
    
    * feat : 기업공지 상세 페이지 기능 개발
    
    - 기업공지 상세 데이터 연동
    - 기업공지 삭제 이벤트 추가

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,7 +8,7 @@ import Profile from '../pages/admin/profile';
 import UserProfile from '../pages/user/profile';
 import ProfileForm from '../pages/admin/profileForm';
 import UserProfileForm from '../pages/user/profileForm';
-import WorkCheck from '../pages/user/work'
+import WorkCheck from '../pages/user/work';
 
 // 페이지 구조 저장 + 렌더링 페이지 정리
 // async : 비동기 데이터 사용 여부
@@ -20,7 +20,7 @@ const routes = {
   //admin
   '/admin/absence': AdminAbsence,
   '/admin/notice': { ...Notice.list, async: true },
-  '/admin/notice/view': Notice.view,
+  '/admin/notice/view/:noticeSn': { ...Notice.view, async: true },
   '/admin/notice/insert': Notice.form,
   '/admin/notice/update': Notice.form,
   '/admin/userList': UserList,
@@ -31,9 +31,9 @@ const routes = {
   '/user/absence': UserAbsence,
   '/user/notice': Notice.list,
   '/user/notice/view': Notice.view,
-  '/user/profile':UserProfile,
-  '/user/profile/profileForm':UserProfileForm,
-  '/user/work':{ ...WorkCheck, async: true }
+  '/user/profile': UserProfile,
+  '/user/profile/profileForm': UserProfileForm,
+  '/user/work': { ...WorkCheck, async: true },
 };
 
 export default routes;
