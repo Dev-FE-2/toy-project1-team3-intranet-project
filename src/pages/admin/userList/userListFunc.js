406e9937adae7dd57ecd0e5de1d0a048a31f56d8
박찬희 <fhfh54321@motionimaging.net>
2024-11-01 07:35:16 +0900

    feat:검색기능 추가

--- src/pages/admin/userList/userListFunc.js
+++ src/pages/admin/userList/userListFunc.js
@@ -1,5 +1,6 @@
 import {pagination} from './userListRender';
 import {renderTableRows} from './userListRender';
+import style from './userList.module.css'
 
 // 임직원 리스트 API 호출 함수
 export const fetchUsers = async (page = 1) => {
@@ -48,21 +49,71 @@ const updateUserList = async (page = 1) =>{
 
   setupPagination(totalPage, page);
 };
+//검색
+// const search = async (searchTerm) => {
+//   const data = await fetchUsers()
+//   const userData = data.data
+//   // searchTerm을 포함하는 항목만 필터링
+//   const filteredUsers = userData.filter(user =>
+//     user.name.includes(searchTerm) || user.email.includes(searchTerm)
+//   );
+//   console.log(filteredUsers)
+//   return filteredUsers
+// }
+//검색 결과 렌더링
+const filterTableRows= (data) =>{
+  const curPath = window.location.pathname;
+  return `
+    <tr onclick="location.href='${curPath}/profile:${data.id}'">
+      <td class="${style.td} checkbox"><input type="checkbox" onclick="event.stopPropagation()"></td>
+      <td class="${style.td} ${style.profileImg}"><img src="${data.image}" alt="${data.name} 프로필사진" class="${style.image}"></td>
+      <td class="${style.td} ${style.name}">${data.name}</td>
+      <td class="${style.td} ${style.email}">${data.email}</td>
+      <td class="${style.td} ${style.phoneNumber}">${data.phoneNumber}</td>
+      <td class="${style.td} ${style.division}">${data.grade ? "관리자": "임직원"}</td>
+   </tr>
+    `
+}
+const filterRender = async(searchTerm) => {
+  const data = await fetchUsers()
+  const userData = data.data
+  const filteredUsers = userData.filter(user =>
+    user.name.includes(searchTerm) || user.email.includes(searchTerm)
+  );
+  const tableRowsHTML = filteredUsers.map(filterTableRows).join('');
+  document.getElementById('userTableBody').innerHTML = tableRowsHTML
+
+  document.getElementById('userCount').textContent = `총 ${filteredUsers.length}명의 임직원`
+
+  setupPagination(1, filteredUsers.length);
 
 
+}
+
+let currentSearchTerm = '';
 const userListFunc = async () => {
   
   const { totalPage } = await fetchUsers();
   const paginationButtons = document.getElementById('paginationButtons');
+  const searchInput = document.getElementById('searchInput');
   //첫 번째 페이지로 초기화
   setupPagination(totalPage, 1);
+  const searchUsers = async () => {
+    currentSearchTerm = searchInput.value.toLowerCase(); // 전역 변수에 검색어 저장
 
+    filterRender(currentSearchTerm)
+  };
   // 페이지네이션 버튼 이벤트 리스너 추가
   paginationButtons.addEventListener('click', (event) => {
    handlePagination(event, totalPage) 
   });
-
-
+  // 검색 이벤트 리스너
+  searchInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      searchUsers();
+    }
+  });
+  
 };
 
 export default userListFunc;
