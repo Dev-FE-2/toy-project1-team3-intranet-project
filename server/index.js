0ccda2daabe5ea31d9e40e27ed6242b5922e0562
Vastia <92559779+laivastia@users.noreply.github.com>
2024-10-31 20:49:36 +0900

    출결신청 페이지 개발 (#59)
    * feat : 출결관리 API 개발
    
    * fix : 출결관리 라우팅 페이지 추가
    
    * feat : 출결관리 페이지 기능 개발

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import noticeCommonRouter from './api/notice/common/index.js';
 import workUserRouter from './api/work/user/index.js';
 import signUpRouter from './api/user/common/userInfo.js';
 import userSignInRouter from './api/user/common/index.js';
+import workOnRouter from './api/work/user/workOn.js';
 
 const THRESHOLD = 2000;
 const port = process.env.PORT || 8080;
@@ -39,7 +40,8 @@ const NOTICE_API_URL = {
 
 const WORK_API_URL = {
   user: `${userPath}/work`,
-}
+  workOn: `${userPath}/workOn`,
+};
 
 app.use(morgan('dev'));
 app.use(express.static('dist'));
@@ -50,6 +52,7 @@ app.use(NOTICE_API_URL.admin, noticeAdminRouter);
 app.use(NOTICE_API_URL.common, noticeCommonRouter);
 app.use(USER_API_URL.signUp, signUpRouter);
 app.use(USER_API_URL.signIn, userSignInRouter);
+app.use(WORK_API_URL.workOn, workOnRouter);
 
 // work 라우터 연결
 app.use(WORK_API_URL.user, workUserRouter);
