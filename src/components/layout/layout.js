28eb346977b033f4042114fbe44164824ac3b1ba
laivastia <zmfwl123@gmail.com>
2024-11-04 12:31:42 +0900

    refact : 사용자 권한(userGrade)에 따른 네비게이션 바 출력 조건문 수정
    - 사용자 권한 정보에 맞춰 렌더링 되는 네비게이션 바 형식 변경
    - shortCut에서 navigate로 명칭 변경
    - 삼항연산자를 사용한 변수형태 에서 함수 - 조건식 형태 렌더링으로 변경

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,24 +1,31 @@
 import style from './layout.module.css';
 import './style.css';
 const Layout = (children) => {
-  const path = window.location.pathname;
-  const shortCut = path.includes('/user/')
-    ? `
-      <a href="/user/workOn">출결신청</a>
-      <a href="/user/work">출근관리</a>
-      <a href="/user/absence">부재관리</a>
-      <a href="/user/notice">공지사항</a>
-      <a href="/user/profile">프로필</a>`
-    : `
-      <a href="/admin/userList">직원관리</a>
-      <a href="/admin/absence">부재관리</a>
-      <a href="/admin/notice">공지사항</a>`;
+  const userGrade = window.localStorage.getItem('userGrade');
+
+  const navigate = () => {
+    if (userGrade === '0') {
+      // 사용자의 권한이 관리자일 경우
+      return `
+    <a href="/admin/userList">직원관리</a>
+    <a href="/admin/absence">부재관리</a>
+    <a href="/admin/notice">공지사항</a>`;
+    } else if (userGrade === '1') {
+      // 사용자의 권한이 임직원일 경우
+      return `
+    <a href="/user/workOn">출결신청</a>
+    <a href="/user/work">출근관리</a>
+    <a href="/user/absence">부재관리</a>
+    <a href="/user/notice">공지사항</a>
+    <a href="/user/profile">프로필</a>`;
+    }
+  };
 
   return /* HTML */ `
     <div class="${style.layout}">
       <header class="${style.header}">
         <button type="button" id="menu" class="${style.hamburger}"></button>
-        ${shortCut}
+        ${navigate()}
         <div class="${style.headerRightWrap}">
           <h1>인트라넷</h1>
           <button
@@ -31,7 +38,7 @@ const Layout = (children) => {
           </button>
         </div>
         <nav id="navigate" class="${style.navigate} hidden">
-          <div class="${style.shortCut}">${shortCut}</div>
+          <div class="${style.shortCut}">${navigate()}</div>
         </nav>
       </header>
       ${children}
